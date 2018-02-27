918e091a06ba2a4b116c6e4b05069220f73ff7de
Krzysztof Dzido <krzysztof.dzido@gmail.com>
2018-02-27 21:27:56 +0100

    - building docker image with bmuschko gradle plugin

--- run-eureka-docker-image.sh
+++ run-eureka-docker-image.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-./gradlew :eureka-server:clean :eureka-server:build && \
-    docker build -t thesis/eurekaserver:latest eureka-server/build/dockerfile && \
-    docker run -ti --rm -p 8761:8761 thesis/eurekaserver:latest
+./gradlew :eureka-server:clean :eureka-server:build :eureka-server:buildDockerImage && \
+    docker run -ti --rm -p 8761:8761 kdzido/eurekaserver:latest
 
 # -ti - interactive mode (ctrl-c) to stop container
 # --rm - container will be removed after stop
