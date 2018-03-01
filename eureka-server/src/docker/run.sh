f7fbc0e2a9f3a82f53f52db821fb5228b0c6dd54
Krzysztof Dzido <krzysztof.dzido@gmail.com>
2018-03-01 22:19:26 +0100

    [THES-1] I want to have Configuration Management server in cluster [THES-12] Create working Cloud Config service

--- eureka-server/src/docker/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo "Starting Eureka service discovery server"
-echo "------------------------------"
-java -Djava.security.egd=file:/dev/./urandom -jar /usr/local/eureka-server/@springBootJar@
--- run-eureka-docker-image.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-./gradlew :eureka-server:clean :eureka-server:build :eureka-server:buildDockerImage && \
-    docker run -ti --rm -p 8761:8761 kdzido/eurekaserver:latest
-
-# -ti - interactive mode (ctrl-c) to stop container
-# --rm - container will be removed after stop
