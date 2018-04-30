6b9c83c5c74d3d8517f1713b11a3d82fae13c7fc
Krzysztof Dzido <xtofek@gmail.com>
2018-04-30 20:18:39 +0200

    [THES-67] Implement a sample service scaling out as Docker Swarm service - registration with IP in Eureka

--- docker-run-sample-service.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-./gradlew :backend-sample-service:clean :backend-sample-service:build :backend-sample-service:buildDockerImage && \
-    docker run -ti --rm -p 8081:8080 kdzido/thesis-sampleservice:latest
-
-# -ti - interactive mode (ctrl-c) to stop container
-# --rm - container will be removed after stop
